53fb0d45394a30b3b73b841c0f3ff724012dbe64
Sjshovan <Sjshovan@Gmail.com>
2019-03-15 01:16:58 -0400

    fix indentation, update readme

--- Clutch.lua
+++ Clutch.lua
@@ -1,179 +1,141 @@
---[[
-Copyright © 2019, Sjshovan (Apogee)
-All rights reserved.
+**Author:** [Sjshovan (Apogee)](https://github.com/Ap0gee)  
+**Version:** v0.9.0  
 
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
 
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright
-      notice, this list of conditions and the following disclaimer in the
-      documentation and/or other materials provided with the distribution.
-    * Neither the name of Clutch nor the
-      names of its contributors may be used to endorse or promote products
-      derived from this software without specific prior written permission.
+# Clutch
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL  Sjshovan (Apogee) BE LIABLE FOR ANY
-DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-]]
+> An Elder Scrolls Online addon that aims to prevent weapons from falling to the ground and grants the ability to pick them back up if they do.
 
-local LSC = LibStub("LibSlashCommander")
 
-if Clutch == nil then Clutch = {} end
+### Table of Contents
 
-Clutch.name = "Clutch"
-Clutch.version = "0.9.0"
-Clutch.author = "Sjshovan (Apogee)"
-Clutch.contact = "Sjshovan@Gmail.com"
-Clutch.description = "Clutch aims to prevent weapons from falling to the ground and grants the ability to pick them back up if they do."
-Clutch.commands = {"/clutch", "/clch"}
+- [Prerequisites](#prerequisites)
+- [Installation](#installation)
+- [Aliases](#aliases)
+- [Usage](#usage)
+- [Commands](#commands)
+- [Support](#support)
+- [Change Log](#change-log)
+- [Known Issues](#known-issues)
+- [TODOs](#todos)
+- [License](#license)
 
-Clutch.collectibles = {
-	none = 0,
-	hats = {
-		hide_your_helm = 5002
-	}
-}
+___
+### Prerequisites
+1. [Elder Scrolls Online](https://www.elderscrollsonline.com/en-us/home)
+2. **[Optional]** : [Minion Addon Manager](https://minion.mmoui.com/)
 
-Clutch.colors = {
-	info = "eeeeee",
-	primary = "9ab3e5",
-	secondary = "6c757d",
-	success = "28a745",
-	warning = "ffc107",
-	danger = "dc3545",
-}
+___
+### Installation
 
-local function cStr(color, str)
-	return string.format("|c%s%s|r", color, str)
-end
+**Minion Addon Manager:**   
 
-local function buildHelpEntryCommand(command, description)
-	local addon_name = cStr(Clutch.colors.primary, "/"..string.lower(Clutch.name)) 
-    local command = cStr(Clutch.colors.info, command)
-    local description = cStr(Clutch.colors.info, description)
-	local sep = cStr(Clutch.colors.primary, "=>")
-    return string.format("%s %s %s %s", addon_name, command, sep, description)
-end
+Not yet available on Minion, installation will have to be done manually.
 
-local function buildHelpEntry(key, value)
-	local key = cStr(Clutch.colors.info, key)
-    local value = cStr(Clutch.colors.info, value)
-	local sep = cStr(Clutch.colors.primary, "=>")
-	return string.format("%s %s %s", key, sep, value)
-end
+**Manual:**   
+1. Navigate to <https://github.com/Ap0gee/Clutch>.
+2. Click on `Releases`. 
+3. Click on the `Source code (zip)` link within the latest release to download.
+4. Extract the zipped folder to `.../Documents/Elder Scrolls Online/live/AddOns`.
+5. Rename the folder to remove the version tag (`-v0.9.0`). The folder should be named `Clutch`.
 
-local function buildHelpTitle(context)
-    local context = cStr(Clutch.colors.info, context)
-	local title = cStr(Clutch.colors.primary, string.format("%s Help:", Clutch.name))
-	return string.format("%s %s", title, context)
-end
 
-local function buildHelpSeperator(character, count)
-    local sep = ''
-    for i = 1, count do
-        sep = sep .. character
-    end
-    return cStr(Clutch.colors.secondary, sep)
-end	
 
-function Clutch.displayHelp(table_help)
-	 for index, command in pairs(table_help) do
-        d(command)
-    end
-end 
+___
+### Aliases
+The following aliases are available to Clutch commands:    
 
-function Clutch.clutchWeapons()	
-	local collectible_hat_active = GetActiveCollectibleByType(COLLECTIBLE_CATEGORY_TYPE_HAT)
+**clutch:** clch   
+**activate:** a    
+**help:** h   
+**info:** i  
+___
+### Usage
+
+Clutch will automatically attempt to keep your weapons off the ground. If the addon is properly loaded, 
+no further action is required.
+
+**Manual:**  
+If you find your weapons fall to the ground, despite Clutch being properly loaded, you may manually activate Clutch with a bound key press or a slash command.  
+
+To bind Clutch to a key press, use the following steps:
+1. Open your main menu
+2. Navigate to `Controls` then `Addon Keybinds`.
+3. Locate Clutch within the list of categories on the left.
+4. Bind Clutch to a key of your choice.
+
+Invoke the Clutch addon with the following commands:
+    
+    /clutch
+    /clch
 	
-	if collectible_hat_active == Clutch.collectibles.none or collectible_hat_active == Clutch.collectibles.hats.hide_your_helm then
-		UseCollectible(Clutch.collectibles.hats.hide_your_helm)
-		UseCollectible(Clutch.collectibles.hats.hide_your_helm)
-	else
-		UseCollectible(Clutch.collectibles.hats.hide_your_helm)
-		UseCollectible(collectible_hat_active)
-	end
-end
+___    
+### Commands 
 
-function Clutch.onBoundKeyPress()
-	d(string.format("%s: %s", 
-		cStr(Clutch.colors.primary, Clutch.name), 
-		cStr(Clutch.colors.info, "Activated!")
-	))
-	
-	Clutch.clutchWeapons()
-end
+**help**
 
-function Clutch.onPlayerActivated()
-	if not Clutch.startup_info_displayed then
-		d(string.format("%s %s %s", 
-			cStr(Clutch.colors.primary, Clutch.name),
-			cStr(Clutch.colors.info, string.format("v%s ~", Clutch.version)), 			
-			cStr(Clutch.colors.primary, Clutch.author)
-		))
-		Clutch.startup_info_displayed = true
-	end	
+Displays the available Clutch commands. Below are the equivalent ways of calling the command:
 	
-	Clutch.clutchWeapons()
-end
+    /clutch help
+    /clch help
+	
+	/clutch h
+	/clch h
+	
+**activate** 
 
-function Clutch.onCommandEntered(args)
-	if args == 'activate' or args == 'a' then
-		Clutch.onBoundKeyPress()
-		
-	elseif args == 'info' or args == 'i' then
-		Clutch.displayHelp(Clutch.help.info)
+Manually activates Clutch and picks up your weapons. Below are the equivalent ways of calling the command:
 	
-	elseif args == 'help' or args == 'h' then
-		Clutch.displayHelp(Clutch.help.commands)
+    /clutch activate
+    /clch activate
 	
-	else 
-		Clutch.displayHelp(Clutch.help.commands)
-	end
-end
+	/clutch a
+    /clch a
+    
+**info**
 
-function Clutch.onAddonLoaded(eventCode, addonName)
-	if addonName ~= Clutch.name then return end
-	
-	EVENT_MANAGER:UnregisterForEvent(Clutch.name, EVENT_ADD_ON_LOADED)
+Display information about Clutch. Below are the equivalent ways of calling the command:  
 
-	ZO_CreateStringId("SI_BINDING_NAME_CLUTCH_WEAPONS", "Clutch Weapons")
-	
-	LSC:Register(Clutch.commands, function(...) Clutch.onCommandEntered(...)  end, "Invoke the Clutch addon")
-	
-	Clutch.help = {
-		commands = {
-			buildHelpSeperator("=", 20),
-			buildHelpTitle("Commands"),
-			buildHelpSeperator('=', 20),
-			buildHelpEntryCommand("[activate, a]",  "Activate Clutch and pick up your weapons."),
-			buildHelpEntryCommand("[help, h]",  "Display Clutch commands."),
-			buildHelpEntryCommand("[info, i]",  "Display information about Clutch."),
-			buildHelpSeperator("=", 20),
-		},
-		info = {
-			buildHelpSeperator("=", 14),
-			buildHelpTitle("Info"),
-			buildHelpSeperator('=', 14),
-			buildHelpEntry("Name", Clutch.name),
-			buildHelpEntry("Description", Clutch.description),
-			buildHelpEntry("Author", Clutch.author),
-			buildHelpEntry("Contact", Clutch.contact),
-			buildHelpEntry("Version", Clutch.version),
-			buildHelpSeperator('=', 14),
-		}
-	}
-	
-	EVENT_MANAGER:RegisterForEvent(Clutch.name, EVENT_PLAYER_ACTIVATED, Clutch.onPlayerActivated)
-end
+    /clutch info
+	/clch info
 
-EVENT_MANAGER:RegisterForEvent(Clutch.name, EVENT_ADD_ON_LOADED, Clutch.onAddonLoaded)
+	/clutch i
+	/clch i
+    
+___
+### Support
+**Having Issues with this addon?**
+* Please let me know [here](https://github.com/Ap0gee/Clutch/issues/new).
+  
+**Have something to say?**
+* Send me some feedback here: <sjshovan@gmail.com>
+
+**Want to stay in the loop with my work?**
+* You can follow me at: <https://twitter.com/Sjshovan>
+
+**Want to show your love and help me make more awesome stuff?**
+* You can do so here: <https://www.paypal.me/Sjshovan>  
+
+___
+### Change Log
+
+**v0.9.0** - 3/15/2019
+- Initial release
+
+___
+### Known Issues
+
+None
+
+___    
+### TODOS
+
+None
+___
+
+### License
+
+Copyright © 2019, [Sjshovan (Apogee)](https://github.com/Ap0gee).
+Released under the [BSD License](LICENSE).
+
+***
